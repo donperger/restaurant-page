a456c8ead897489c987e8299145d6946daf7452a
Rafael Perger <rafael.perger@gmail.com>
2022-03-13 18:37:20 +0100

    Reorder the elements.

--- src/contentCreator.js
+++ src/contentCreator.js
@@ -1,16 +1,16 @@
 function createContent() {
     const contentDiv = document.getElementById("content");
 
+    const header = document.createElement("h1");
+    header.textContent = 'Restaurant for humans and their best friends';
+    contentDiv.appendChild(header);
+
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("img-container");
     const img = document.createElement("img");
     img.src = '../img/karsten-winegeart-qy0BHykaq0E-unsplash (1).jpg';
     imgContainer.appendChild(img);
     contentDiv.appendChild(imgContainer);
-
-    const header = document.createElement("h1");
-    header.textContent = 'Restaurant for humans and their best friends';
-    contentDiv.appendChild(header);
 
     const description = document.createElement("div");
     description.textContent = 'Our restaurant is a animal friendly place. If you want to have a prefect meal with your best friends (humans and/or animals) just come to us and order some delicous dish for all of you.';
