8be7b247383f57de1772c244cb88c7b1cbc2ab5c
Rafael Perger <rafael.perger@gmail.com>
2022-03-14 16:14:42 +0100

    Add event listeners to header buttons. Create navigateToHome function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import createContent from "./contentCreator";
 
+const contentDiv = document.getElementById("content");
+
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const contactBtn = document.getElementById("contact-btn");
+
+homeBtn.addEventListener("click", navigateToHome);
+menuBtn.addEventListener("click", () => console.log("menu button"));
+contactBtn.addEventListener("click", () => console.log("contact button"));
+
 createContent();
+
+function navigateToHome() {
+    contentDiv.textContent = "";
+    createContent();
+}
