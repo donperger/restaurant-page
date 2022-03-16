5f7121e5d51d1bd4141139a9745131067f39c7e9
Rafael Perger <rafael.perger@gmail.com>
2022-03-16 10:12:48 +0100

    Delete unnecessary comment.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        // More information here https://webpack.js.org/guides/asset-modules/
         type: "asset",
       },
     ],
